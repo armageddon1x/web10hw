c3c79cb610bdc6536167a1baf02d22d4696a3015
armageddon <armageddon1x@gmail.com>
2015-08-23 12:45:29 -0400

    added other pages, finishing about page

--- js/function.js
+++ js/function.js
@@ -30,5 +30,16 @@ $(document).ready(function(){
     //  });
   });
 
+  //google map
+  function initialize() {
+        var mapCanvas = document.getElementById('map');
+        var mapOptions = {
+          center: new google.maps.LatLng(40.748817, -73.985428),
+          zoom: 17,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+        var map = new google.maps.Map(mapCanvas, mapOptions)
+      }
+      google.maps.event.addDomListener(window, 'load', initialize);
 	//end
 });
