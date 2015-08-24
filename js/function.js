6646a2cc664c02829b2631cbbf19097c1d9b1dc7
armageddon <armageddon1x@gmail.com>
2015-08-23 20:39:17 -0400

    minor edits

--- js/function.js
+++ js/function.js
@@ -32,13 +32,45 @@ $(document).ready(function(){
 
   //google map
   function initialize() {
-        var mapCanvas = document.getElementById('map');
-        var mapOptions = {
-          center: new google.maps.LatLng(40.748817, -73.985428),
-          zoom: 17,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        }
-        var map = new google.maps.Map(mapCanvas, mapOptions)
+    var mapCanvas = document.getElementById('map_canvas');
+    var mapOptions = {
+      center: new google.maps.LatLng(40.748817, -73.985428),
+      zoom: 17,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    var map = new google.maps.Map(mapCanvas, mapOptions)
+
+    var styles = [
+      {"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},
+      {"color":"#000000"},
+      {"lightness":40}]},
+      {"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},
+      {"color":"#000000"},{"lightness":16}]},  
+      {"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},
+      {"lightness":20}]},
+      {"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},
+      {"lightness":17},
+      {"weight":1.2}]},
+      {"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},
+      {"lightness":20}]},
+      {"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},
+      {"lightness":21}]},
+      {"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},
+      {"lightness":17}]},
+      {"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},
+      {"lightness":29},
+      {"weight":0.2}]},
+      {"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},
+      {"lightness":18}]},
+      {"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},
+      {"lightness":16}]},
+      {"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},
+      {"lightness":19}]},
+      {"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"},
+      {"lightness":17}]}
+    ]
+    map.setOptions({styles: styles});
+    
       }
       google.maps.event.addDomListener(window, 'load', initialize);
 	//end
